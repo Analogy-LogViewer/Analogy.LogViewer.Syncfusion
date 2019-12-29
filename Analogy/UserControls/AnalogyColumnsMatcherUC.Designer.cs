0b1aafb571048ca6b4a011dc9cebe764f0ced08b
Lior Banai <liorbanai@gmail.com>
2019-12-29 21:44:50 +0200

    user controls

--- Analogy/UserControls/AnalogyColumnsMatcherUC.Designer.cs
+++ Analogy/UserControls/AnalogyColumnsMatcherUC.Designer.cs
@@ -61,7 +61,7 @@ namespace Analogy.UserControls
             this.spltNlogParser.Panel2.Controls.Add(this.labelControl10);
             this.spltNlogParser.Panel2.Text = "Panel2";
             this.spltNlogParser.Size = new System.Drawing.Size(688, 483);
-            this.spltNlogParser.SplitterPosition = 191;
+            this.spltNlogParser.SplitterPosition = 211;
             this.spltNlogParser.TabIndex = 10;
             // 
             // lstBAnalogyColumns
@@ -89,16 +89,16 @@ namespace Analogy.UserControls
             "__ignore__",
             "__ignore__",
             "__ignore__"});
-            this.lstBAnalogyColumns.Location = new System.Drawing.Point(44, 16);
+            this.lstBAnalogyColumns.Location = new System.Drawing.Point(65, 16);
             this.lstBAnalogyColumns.Name = "lstBAnalogyColumns";
-            this.lstBAnalogyColumns.Size = new System.Drawing.Size(147, 463);
+            this.lstBAnalogyColumns.Size = new System.Drawing.Size(146, 463);
             this.lstBAnalogyColumns.TabIndex = 0;
             this.lstBAnalogyColumns.SelectedIndexChanged += new System.EventHandler(this.lstBAnalogyColumns_SelectedIndexChanged);
             // 
             // labelControl9
             // 
             this.labelControl9.Dock = System.Windows.Forms.DockStyle.Top;
-            this.labelControl9.Location = new System.Drawing.Point(44, 0);
+            this.labelControl9.Location = new System.Drawing.Point(65, 0);
             this.labelControl9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.labelControl9.Name = "labelControl9";
             this.labelControl9.Size = new System.Drawing.Size(128, 16);
@@ -116,34 +116,45 @@ namespace Analogy.UserControls
             this.splitContainerControl2.Panel1.Text = "Panel1";
             this.splitContainerControl2.Panel2.Controls.Add(this.sBtnMoveDown);
             this.splitContainerControl2.Panel2.Text = "Panel2";
-            this.splitContainerControl2.Size = new System.Drawing.Size(44, 479);
+            this.splitContainerControl2.Size = new System.Drawing.Size(65, 479);
             this.splitContainerControl2.SplitterPosition = 237;
             this.splitContainerControl2.TabIndex = 1;
             // 
             // sBtnMoveUp
             // 
+            this.sBtnMoveUp.AccessibleName = "Button";
             this.sBtnMoveUp.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.sBtnMoveUp.Image = ((System.Drawing.Image)(resources.GetObject("sBtnMoveUp.ImageOptions.Image")));
-            this.sBtnMoveUp.TextImageRelation = TextImageRelation.TextAboveImage;
+            this.sBtnMoveUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sBtnMoveUp.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sBtnMoveUp.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.sBtnMoveUp.ImageSize = new System.Drawing.Size(30, 30);
             this.sBtnMoveUp.Location = new System.Drawing.Point(0, 0);
             this.sBtnMoveUp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.sBtnMoveUp.Name = "sBtnMoveUp";
-            this.sBtnMoveUp.Size = new System.Drawing.Size(44, 237);
+            this.sBtnMoveUp.Size = new System.Drawing.Size(65, 237);
+            this.sBtnMoveUp.Style.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
             this.sBtnMoveUp.TabIndex = 2;
             this.sBtnMoveUp.Text = "Up";
+            this.sBtnMoveUp.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.sBtnMoveUp.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.sBtnMoveUp.Click += new System.EventHandler(this.SBtnMoveUp_Click);
             // 
             // sBtnMoveDown
             // 
+            this.sBtnMoveDown.AccessibleName = "Button";
             this.sBtnMoveDown.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.sBtnMoveDown.Image = ((System.Drawing.Image)(resources.GetObject("sBtnMoveDown.ImageOptions.Image")));
-            this.sBtnMoveDown.TextImageRelation = TextImageRelation.TextBeforeImage;
+            this.sBtnMoveDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sBtnMoveDown.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sBtnMoveDown.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.sBtnMoveDown.ImageSize = new System.Drawing.Size(30, 30);
             this.sBtnMoveDown.Location = new System.Drawing.Point(0, 0);
             this.sBtnMoveDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.sBtnMoveDown.Name = "sBtnMoveDown";
-            this.sBtnMoveDown.Size = new System.Drawing.Size(44, 236);
+            this.sBtnMoveDown.Size = new System.Drawing.Size(65, 236);
+            this.sBtnMoveDown.Style.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image1")));
             this.sBtnMoveDown.TabIndex = 3;
             this.sBtnMoveDown.Text = "Down";
+            this.sBtnMoveDown.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.sBtnMoveDown.Click += new System.EventHandler(this.SBtnMoveDown_Click);
             // 
             // lstBoxItemsNlog
@@ -151,7 +162,7 @@ namespace Analogy.UserControls
             this.lstBoxItemsNlog.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstBoxItemsNlog.Location = new System.Drawing.Point(0, 16);
             this.lstBoxItemsNlog.Name = "lstBoxItemsNlog";
-            this.lstBoxItemsNlog.Size = new System.Drawing.Size(487, 463);
+            this.lstBoxItemsNlog.Size = new System.Drawing.Size(467, 463);
             this.lstBoxItemsNlog.TabIndex = 2;
             // 
             // labelControl10
